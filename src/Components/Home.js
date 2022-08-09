790b634600b2a8b78f4202d09bbc9b839f1c4034
Precious <Preshomondi@gmail.com>
2022-08-10 00:40:08 +0300

    Create components

---
+++ src/Components/Home.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./Home.css"
+import Cards from "./Cards"
+
+function Home(){
+    return(
+        <div class="container">
+
+        </div>
+    )
+
+}
+
+export default Home
